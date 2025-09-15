3edd834b25d44dce4f8f228582b14cbcef631f63
Gustavo Pineda <gpine035@fiu.edu>
2025-09-15 18:16:55 -0400

    Initial Commit

---
+++ Scavenger Hunt/Scavenger_HuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scavenger_HuntApp.swift
+//  Scavenger Hunt
+//
+//  Created by Gustavo Pineda on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Scavenger_HuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Scavenger HuntTests/Scavenger_HuntTests.swift
@@ -0,0 +1,17 @@
+//
+//  Scavenger_HuntTests.swift
+//  Scavenger HuntTests
+//
+//  Created by Gustavo Pineda on 9/15/25.
+//
+
+import Testing
+@testable import Scavenger_Hunt
+
+struct Scavenger_HuntTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
